0e33f87c748452ab8da48816b40fd752547176f4
IvanGalenko <98175502+IvanGalenko@users.noreply.github.com>
2023-03-08 18:34:18 +0300

    Код дополнен

---
+++ BLL/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using SocialNetwork.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using SocialNetwork.DAL.Repositories;
+using SocialNetwork.DAL.Entities;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class UserService
+    {
+        IUserRepository userRepository;
+        public UserService()
+        {
+            userRepository = new UserRepository();
+        }
+
+        public void Register(UserRegistrationData userRegistrationData)
+        {
+            if (string.IsNullOrEmpty(userRegistrationData.FirstName)) throw new ArgumentNullException();
+            if (string.IsNullOrEmpty(userRegistrationData.LastName)) throw new ArgumentNullException();
+            if (string.IsNullOrEmpty(userRegistrationData.Password)) throw new ArgumentNullException();
+            if (string.IsNullOrEmpty(userRegistrationData.Email)) throw new ArgumentNullException();
+            if (userRegistrationData.Password.Length < 8) throw new ArgumentNullException();
+            if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email)) throw new ArgumentNullException();
+            if (userRepository.FindByEmail(userRegistrationData.Email) != null) throw new ArgumentNullException();
+            var userEntity = new UserEntity()
+            {
+                firstname = userRegistrationData.FirstName,
+                lastname = userRegistrationData.LastName,
+                password = userRegistrationData.Password,
+                email = userRegistrationData.Email
+            };
+            if (this.userRepository.Create(userEntity) == 0) throw new Exception(); 
+        }
+    }
+}
